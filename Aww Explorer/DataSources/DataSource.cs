9fb7278d21868c0ab2a72cc9a79608e8337397e3
davidmarcbrown <david@davidmarcbrown.com>
2015-05-01 10:58:33 -0700

    commit solution

---
+++ Aww Explorer/DataSources/DataSource.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Data.Json;
+
+namespace Aww_Explorer.DataSources
+{
+    public class RedditGridViewItem
+    {
+        public string ID {get; set;}
+        public string Title { get; set; }
+        public string Link { get; set; }
+        public string Thumbnail { get; set; }
+        public string Author { get; set; }
+    }
+
+    public class DataSource
+    {
+        public ObservableCollection<RedditGridViewItem> Posts;
+        private HttpClient httpClient;
+
+        public DataSource()
+        {
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Add("user-agent", "A demo app written with .Net");
+
+            Posts = new ObservableCollection<RedditGridViewItem>();
+        }
+
+        public async void PopulatePosts(string subreddit)
+        {
+            string requestUrl = "http://reddit.com/r/" + subreddit + ".json";
+            string jsonText;
+
+            HttpResponseMessage response = await httpClient.GetAsync(requestUrl);
+            response.EnsureSuccessStatusCode();
+
+            jsonText = await response.Content.ReadAsStringAsync();
+
+            JsonValue page = JsonValue.Parse(jsonText);
+
+            string kind = page.GetObject().GetNamedString("kind");
+
+            if (kind.Equals("Listing"))
+            {
+                JsonArray children = page.GetObject().GetNamedObject("data").GetNamedArray("children");
+                int count = children.Count();
+
+                if (count > 0)
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        RedditGridViewItem post = new RedditGridViewItem();
+
+                        JsonObject jsonPost = children.GetObjectAt((uint)i).GetNamedObject("data");
+                        bool isSelf = jsonPost.GetNamedBoolean("is_self");
+                        string thumb = jsonPost.GetNamedString("thumbnail");
+
+                        if (!isSelf && !thumb.Equals("default"))
+                        {
+                            post.Title = jsonPost.GetNamedString("title");
+                            post.ID = jsonPost.GetNamedString("id");
+                            post.Author = jsonPost.GetNamedString("author");
+                            post.Thumbnail = thumb;
+                            post.Link = jsonPost.GetNamedString("url");
+                            Posts.Add(post);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Aww Explorer/MainPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Aww_Explorer.DataSources;
+using System.Collections.Specialized;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace Aww_Explorer
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        DataSource dataSource;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+
+            dataSource = new DataSources.DataSource();
+            dataSource.PopulatePosts("Aww");
+            gridView.DataContext = dataSource.Posts;   
+        }
+
+        private void onCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            //info.Text = dataSource.Posts.Count().ToString();
+            var posts = dataSource.Posts;
+            if (posts.Count > 0)
+            {
+                info.Text = posts[0].Title;
+            }
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+        }
+
+        private void changesub_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
